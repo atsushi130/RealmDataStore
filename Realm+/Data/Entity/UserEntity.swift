b0d9923cbabb80f640256679bc035e3dcf2b37ff
atsushi130 <atsushi_main@outlook.jp>
2017-06-25 15:34:50 +0900

    implemented UserEntity

--- Realm+/Data/Entity/UserEntity.swift
+++ Realm+/Data/Entity/UserEntity.swift
@@ -2,8 +2,36 @@
 //  UserEntity.swift
 //  Realm+
 //
-//  Created by 三宅　篤史 on 2017/06/25.
+//  Created by Atsushi Miyake on 2017/06/25.
 //  Copyright © 2017年 Atsushi Miyake. All rights reserved.
 //
 
 import Foundation
+import RealmSwift
+import Realm
+
+final class UserEntity: RealmEntity {
+    
+    dynamic var name: String = ""
+    dynamic var age:  Int    = 0
+    dynamic var companyId: String = ""
+    
+    init(user: User) {
+        super.init(id: user.id)
+        self.name      = user.name
+        self.age       = user.age
+        self.companyId = user.companyId
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    required init(value: Any, schema: RLMSchema) {
+        super.init(value: value, schema: schema)
+    }
+    
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init(realm: realm, schema: schema)
+    }
+}
