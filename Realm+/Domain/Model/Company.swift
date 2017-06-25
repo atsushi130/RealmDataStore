557cafec19af47464a74158002224e587370ae5c
atsushi130 <atsushi_main@outlook.jp>
2017-06-25 15:35:21 +0900

    implemented Company

--- Realm+/Domain/Model/Company.swift
+++ Realm+/Domain/Model/Company.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+final class Company {
+    let id: String
+    let name: String
+    
+    init(id: String, name: String) {
+        self.id   = id
+        self.name = name
+    }
+}
